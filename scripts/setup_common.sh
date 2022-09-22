70b04516b3df89674a8953d82d6b7a616ad5568f
Kondou,Nobuhiro <kondou.nobuhiro@gmail.com>
2022-09-21 22:46:40 -0500

    gitconfig

--- scripts/setup_common.sh
+++ scripts/setup_common.sh
@@ -4,26 +4,31 @@ set -eu
 
 dotfiles_dir=${HOME}/dotfiles
 
-if [ -f ${dotfiles_dir}/zshrc ]
+if [ -f ${dotfiles_dir}/zshrc ]; then
     mkdir -p ${HOME}/.config/zsh
-    ln -s ${dotfiles_dir}/zshrc ${HOME}/.config/zsh/zshrc
+    ln -sf ${dotfiles_dir}/zshrc ${HOME}/.config/zsh/zshrc
     echo 'source ${HOME}/.config/zsh/zshrc' >> ${HOME}/.zshrc
 fi
 
-if [ -f ${dotfiles_dir}/alacritty.yml ]
-    ln -s ${dotfiles_dir}/alacritty.yml ${HOME}/.config/alacritty/
+if [ -f ${dotfiles_dir}/alacritty.yml ]; then
+    mkdir -p ${HOME}/.config/alacritty
+    ln -sf ${dotfiles_dir}/alacritty.yml ${HOME}/.config/alacritty/
 fi
 
-if [ -f ${dotfiles_dir}/starship.toml ]
-    ln -s ${dotfiles_dir}/starship.toml ${HOME}/.config/
+if [ -f ${dotfiles_dir}/starship.toml ]; then
+    ln -sf ${dotfiles_dir}/starship.toml ${HOME}/.config/
     echo 'eval "$(starship init zsh)"' >> ${HOME}/.zshrc
 fi
 
-if [ -f ${dotfiles_dir}/tmux.conf ]
-    ln -s ${dotfiles_dir}/tmux.conf ${HOME}/.tmux.conf
+if [ -f ${dotfiles_dir}/tmux.conf ]; then
+    ln -sf ${dotfiles_dir}/tmux.conf ${HOME}/.tmux.conf
 fi
 
-if [ -f ${dotfiles_dir}/vimrc ]
-    ln -s ${dotfiles_dir}/vimrc ${HOME}/.vimrc
+if [ -f ${dotfiles_dir}/vimrc ]; then
+    ln -sf ${dotfiles_dir}/vimrc ${HOME}/.vimrc
+fi
+
+if [ -f ${dotfiles_dir}/gitconfig ]; then
+    ln -sf ${dotfiles_dir}/gitconfig ${HOME}/.gitconfig
 fi
 
--- scripts/setup_linux.sh
+++ scripts/setup_linux.sh
@@ -2,9 +2,9 @@
 
 set -eu
 
-apt install zsh-autosuggestions
-if [ -f /usr/share/zsh-autosuggestions/zsh-autosuggestions.zsh ]
-    echo "source /usr/sharezsh-autosuggestions/zsh-autosuggestions.szh" >> ${HOME}/.zshrc
+sudo apt install zsh-autosuggestions
+if [ -f /usr/share/zsh-autosuggestions/zsh-autosuggestions.zsh ]; then
+    echo "source /usr/share/zsh-autosuggestions/zsh-autosuggestions.zsh" >> ${HOME}/.zshrc
 fi
 
-apt install tmux
+sudo apt install tmux
